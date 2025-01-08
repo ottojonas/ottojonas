f78ff43ad729cd2778bf53795f6a3455acc20471
ottojonas <ottobjonas@icloud.com>
2025-01-08 10:27:17 +0000

    hide_progress

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Fetch GitHub stats
 curl -o assets/stats.svg "https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27&include_all_commits=true"
-curl -o assets/languages.svg "https://github-readme-stats.vercel.app/api/top-langs?username=ottojonas&show_icons=true&theme=transparent&hide_border=true"
+curl -o assets/languages.svg "https://github-readme-stats.vercel.app/api/top-langs?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&hide_progess=true"
 
 # Fetch WakaTime badge
 curl -o assets/wakatime.svg "https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social"
