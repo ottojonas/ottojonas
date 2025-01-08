160c5a2515e8314a3c59b5e5a59955cee98dd529
otto <ottobjonas@outlook.com>
2025-01-08 18:20:25 +0000

    Update fetch_stats.sh

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fetch GitHub stats
-curl -o assets/stats.svg "https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27&include_all_commits=true"
+curl -o assets/stats.svg "https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27"
 curl -o assets/languages.svg "https://github-readme-stats.vercel.app/api/top-langs?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&layout=compact"
 
 # Fetch WakaTime badge
