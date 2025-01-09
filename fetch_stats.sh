fa8a6b18866ab11571928b51c3f2fce549942190
ottojonas <ottojonas@outlook.com>
2025-01-09 23:22:11 +0000

    stats update

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,4 +1,4 @@
+#!/bin/zsh
 
-dos2unix fetch_stats.sh
-wget 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27' -O assets/stats.svg
+wget 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27&show_all_commits=true' -O assets/stats.svg
 wget 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&theme=transparent&hide_border=true&langs_count=3' -O assets/languages.svg
