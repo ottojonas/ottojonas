3913f64b3b921c04e1252dce7df3c3fbfe4d1637
ottojonas <ottojonas@outlook.com>
2025-01-09 17:04:44 +0000

    commits and that

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,4 +1,5 @@
+
+dos2unix fetch_stats.sh
 wget 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27' -O assets/stats.svg
 wget 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&layout=compact&theme=transparent&hide_border=true' -O assets/languages.svg
-wget assets/wakatime.svg 'https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social' -O assets/wakatime.svg
-
+wget 'https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social' -O assets/wakatime.svg
