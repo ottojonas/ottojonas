b30a56d1a7770f037a3efca468328cef816e3353
ottojonas <ottojonas@outlook.com>
2025-01-09 16:54:34 +0000

    commit

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-# Fetch GitHub stats
-curl -o assets/stats.svg "https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27"
-curl -o assets/languages.svg "https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&layout=compact&theme=transparent"
+wget 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27' -O assets/stats.svg
+wget 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&layout=compact&theme=transparent&hide_border=true' -O assets/languages.svg
+wget assets/wakatime.svg 'https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social' -O assets/wakatime.svg
 
-# Fetch WakaTime badge
-curl -o assets/wakatime.svg "https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social"
