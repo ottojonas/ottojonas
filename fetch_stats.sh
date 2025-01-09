42b66417fc9ae15720ddfc58cc6beb202b4b423c
ottojonas <ottojonas@outlook.com>
2025-01-09 22:35:42 +0000

    update

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,5 +1,5 @@
 
 dos2unix fetch_stats.sh
 wget 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27' -O assets/stats.svg
-wget 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&layout=compact&theme=transparent&hide_border=true' -O assets/languages.svg
+wget 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&layout=compact&theme=transparent&hide_border=true&langs_count=3' -O assets/languages.svg
 wget 'https://wakatime.com/badge/user/6301085b-3500-4984-b7b3-0090a149006e.svg?style=social' -O assets/wakatime.svg
