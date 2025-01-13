73e787207dbf57472201be6f6f4218c8d8734b55
ottojonas <ottobjonas@icloud.com>
2025-01-13 22:39:37 +0000

    updated stats

--- fetch_stats.sh
+++ fetch_stats.sh
@@ -1,2 +1,2 @@
-curl -o assets/stats.svg 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27&rank_icon=github'
+curl -o assets/stats.svg 'https://github-readme-stats.vercel.app/api?username=ottojonas&show_icons=true&theme=transparent&hide_border=true&line_height=27&rank_icon=github&show_all_commits=true'
 curl -o assets/languages.svg 'https://github-readme-stats.vercel.app/api/top-langs/?username=ottojonas&theme=transparent&hide_border=true&langs_count=6&hide_progress=true'
